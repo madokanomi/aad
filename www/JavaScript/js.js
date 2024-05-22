f2d8d401c3df4c554cde6422ac9e5bb7cfab2c08
madokanomi <josepereiratuamae@gmail.com>
2024-05-22 12:33:18 +0900

    atualizacaonovagames

--- www/JavaScript/js.js
+++ www/JavaScript/js.js
@@ -5,3 +5,21 @@ function abrirPagina() {
 function voltar(){
     window.location.href = 'index.html';
 }
+
+function assentoesq(){
+    window.location.href = 'assentoesq.html';
+}
+function assentodireit(){
+    window.location.href = 'assentodireit.html';
+}
+function assentomeio(){
+    window.location.href = 'assentomeio.html';
+}
+
+function assentos(){
+    window.location.href = 'assentos.html';
+}
+
+function voltar2(){
+    window.location.href = 'index2.html';
+  }
